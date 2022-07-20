faafa7663aa02c20dc163222b65815951446f79f
NOTHINGYQ <yliu9900@usc.edu>
2022-07-20 01:50:02 -0700

    add content for about

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -7,64 +7,63 @@ import {
     Text,
     HStack,
     VStack,
-    useBreakpointValue,
+    useBreakpointValue, Box,
 } from '@chakra-ui/react';
 import Nav from "../NavBar/nav";
 import Footer from "../footer/footer";
+import './about.css';
 
 
-export default function SplitScreen() {
+export default function About() {
     return (
-        <div>
+        <>
             <Nav/>
-            <VStack>
-            <Stack minH={'60vh'} direction={{ base: 'column', md: 'row' }}>
-                <Flex p={8} flex={1} align={'top'} justify={'center'}>
-                    <Stack spacing={6} w={'full'} maxW={'lg'}>
-                        <Heading fontSize={{ base: '3xl', md: '4xl', lg: '5xl' }}>
-
-                            <Text color={'blue.400'} as={'span'}>
-                                About Me
-                            </Text>{' '}
-                        </Heading>
-                        <Text fontSize={{ base: 'md', lg: 'lg' }} color={'gray.500'}>
-                            The project board is an exclusive resource for contract work. It's
-                            perfect for freelancers, agencies, and moonlighters.Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-                        </Text>
-
-                    </Stack>
-                </Flex>
-                <Flex flex={1} align={'center'} justify={'center'}>
-                    <Image
-                        alt={'Login Image'}
-                        objectFit={'cover'}
-                        width='50%'
-                        height='50%'
-                        src={
-                            'https://images.unsplash.com/photo-1527689368864-3a821dbccc34?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
-                        }
-                    />
-                </Flex>
-            </Stack>
-                <VStack width={'87%'}>
-                    <Text fontSize={{ base: 'md', lg: 'lg' }} color={'gray.500'}>
-                        The project board is an exclusive resource for contract work. It's
-                        perfect for freelancers, agencies, and moonlighters.The project board is an exclusive resource for contract work. It's
-                        perfect for freelancers, agencies, and moonlighters.
-                    </Text>
-                    <Text fontSize={{ base: 'md', lg: 'lg' }} color={'gray.500'}>
-                        The project board is an exclusive resource for contract work. It's
-                        perfect for freelancers, agencies, and moonlighters.moonlighters.Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "L
-                    </Text>
-                    <Text fontSize={{ base: 'md', lg: 'lg' }} color={'gray.500'}>
-                        The project board is an exclusive resource for contract work. It's
-                        perfect for freelancers, agencies, and moonlighters.moonlighters.Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "L
-                    </Text>
-                </VStack>
-
-
-            </VStack>
+            <section className="about">
+                <section id="about-info" className="py-3">
+                    <div className="container">
+                        <div className="info-left">
+                            <Heading fontSize={{ base: '3xl', md: '4xl', lg: '5xl' }}>
+                                <Text color={'blue.400'} as={'span'}>
+                                    About Me
+                                </Text>{' '}
+                            </Heading>
+                            <span style={{ fontWeight: 'bold' }}>Yu Liu</span> was
+                            born in Hebei, China, in 1998. In 2017, he matriculated
+                            at the Tianjin University in Education. One year later,
+                            he switched to Software Engineering. Over three years,
+                            he has grown up to be a skillful programmer, with interests
+                            and experience in various fields of Computer Science,
+                            including website development, backend development,
+                            and mobile development.
+                        </div>
+                        <div className="info-right">
+                            <img src={'https://images.unsplash.com/photo-1527689368864-3a821dbccc34?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'} alt="profile" />
+                        </div>
+                        <div className="info-below">
+                            In addition to excellence in Software Engineering, his ambition
+                            drives him to pursue accomplishments in other fields. To seek
+                            opportunities to combine his skills in both programming and
+                            management, he took his second major in Financial Management.
+                            This opportunity served as a start to gain knowledge beyond
+                            the scope of engineers and will further facilitate him to be
+                            a manager with a good sense of business.
+                        </div>
+                        <div className="info-below">
+                            To improve his scientific research ability, he joined the research assistant program of the Chinese Academy of Sciences during the winter vacation of his junior year to help his tutor handle a project on a graphic search engine. This scientific research experience gave him a deeper understanding of the construction of the residual network model.
+                        </div>
+                        <div className="info-below">
+                            To improve his engineering development capabilities, he joined the internship program of NeuEdu in 2020 and participated in the development of a knowledge graph project. This internship taught him to use the front and back ends to build a complete engineering project.
+                        </div>
+                        <div className="info-below">
+                            A strong academic background and ability to always learn new technology have catalyzed his pursuit of higher goals. Since his junior year, he frequently participated in extra-curricular activities to strengthen and expand his programming capabilities. In 2020, he led two teammates to participate in the China Computer Competition- Artificial Intelligence Challenge and won the national third prize.
+                        </div>
+                        <div className="info-below">
+                            Currently, he is doing an internship at the USC Center for Systems and Software Engineering. And he is looking for an internship in Software Development in the U.S. Summer 2023 with work authorization.
+                        </div>
+                    </div>
+                </section>
+            </section>
             <Footer/>
-        </div>
+        </>
     );
 }
